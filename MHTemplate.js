14704128f9a2bbab22227ff819dcaa87a1242ccb
masanorythm <masanorythm@gmail.com>
2015-12-19 14:31:43 +0900

    Add template file

---
+++ MHTemplate.js
@@ -0,0 +1,18 @@
+//=============================================================================
+// MHPlugin - Template
+// MHTemplate.js
+//=============================================================================
+
+/*:
+ * @plugindesc Plugin description
+ *
+ * @author masanorythm
+ *
+ * @param Parameter description
+ * @desc Parameter description
+ *
+ */
+
+ (function() {
+
+ })();
